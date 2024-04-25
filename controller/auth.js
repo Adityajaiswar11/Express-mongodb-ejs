a9345bf7a570cf09bbb8d6f0d94ad331e1ba16e8
Aditya Jaiswar <adityajaiswar11@gmail.com>
2024-04-24 23:12:29 -0800

    crud

--- controller/auth.js
+++ controller/auth.js
@@ -1,80 +1,79 @@
-const User = require("../model/user");
-
+const User = require('../model/user')
 
 const createUser = (req, res) => {
-    res.render("index")
+  res.render('index')
 }
 
-
-
 const postUser = async (req, res) => {
-    try {
-       const user = await new User(req.body);
-       const {email} = user;
-       const isEmailExist = await User.findOne({email});
-       if(isEmailExist) return res.status(400).send('Email already exist'); 
-       await user.save();
-       res.redirect("/users")
-      //  return res.status(201).json({message:"data stored successfully",data:savaData});
-    } catch (error) {
-          console.log(error)
-          return res.status(500).json({message: "Internal Server Error"})
-    }
-   
-};
+  try {
+    const user = await new User(req.body)
+    const { email } = user
+    const isEmailExist = await User.findOne({ email })
+    if (isEmailExist) return res.status(400).send('Email already exist')
+    await user.save()
+    res.redirect('/users')
+    //  return res.status(201).json({message:"data stored successfully",data:savaData});
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
 
 const readuser = async (req, res) => {
   try {
-  let user =  await User.find();
-    res.render("user",{user})
+    let user = await User.find()
+    res.render('user', { user })
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ msg: "server error", error: error });
+    console.log(error)
+    return res.status(500).json({ msg: 'server error', error: error })
   }
-};
+}
 
-const updataEjs = async(req,res)=>{
-    id = req.params.id;
-    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-    if(!user) return res.redirect("/")
-    await user.save()
-    res.render(`update`,{user})
+const updataEjs = async (req, res) => {
+  id = req.params.id
+  const user = await User.findByIdAndUpdate(id, req.body, { new: true })
+  if (!user) return res.redirect('/')
+  await user.save()
+  res.render(`update`, { user })
 }
 const update = async (req, res) => {
   try {
-    const id = req.params.id;
-    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+    const id = req.params.id
+    const user = await User.findByIdAndUpdate(id, req.body, { new: true })
     await user.save()
-    res.redirect("/users")
+    res.redirect('/users')
     // return res.status(20  res.redirect("/")0).json({ msg: "success", status: 200, data: user });
   } catch (error) {
-    console.log(error);
-    return res
-      .status(500)
-      .json({ message: "Error", error: error, status: 400 });
+    console.log(error)
+    return res.status(500).json({ message: 'Error', error: error, status: 400 })
   }
-};
-const deleteById = async(req, res) => {
+}
+const deleteById = async (req, res) => {
   try {
-    const id = req.params.id;
-  await User.findByIdAndDelete(id);
-   return res.redirect("/users")
+    const id = req.params.id
+    await User.findByIdAndDelete(id)
+    return res.redirect('/users')
     // return res
     //   .status(200)
     //   .json({ msg: "data deleted successfully", status: 200 });
   } catch (error) {
-    console.log(error);
-    return res
-      .status(400)
-      .json({ message: "Error", error: error, status: 400 });
+    console.log(error)
+    return res.status(400).json({ message: 'Error', error: error, status: 400 })
   }
-};
-
-const signup = (req,res)=>{
-     return res.render('signup')
 }
 
-const login = (req,res)=>{
-  return res.render('login')
+// const signup = (req, res) => {
+//   return res.render('signup')
+// }
+
+// const login = (req, res) => {
+//   return res.render('login')
+// }
+module.exports = {
+  postUser,
+  readuser,
+  update,
+  createUser,
+  deleteById,
+  updataEjs,
 }
-module.exports = { postUser, readuser, update,signup,login ,createUser,deleteById,updataEjs};
